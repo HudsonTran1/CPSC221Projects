856f9b5a72e5fef3c5dc4fca2b495a3a6982bfda
HudsonTran1 <hudsontran@my.cwi.edu>
2025-04-03 10:32:22 -0600

    2 element tests [5/5] Fixes #15 I have added all the tests for this block. I also tested with the goodList for Scenario 6 using a temp implementation of emptyList_addToFrontA_A and it passed all tests.
    SCENARIO: A_addToFrontB_BA
    
    toString() output: [2, 1]
    
    Section Tests: 8,  Passed: 8,  Failed: 0

--- Lab08_Lists_Pt2/ListTester.java
+++ Lab08_Lists_Pt2/ListTester.java
@@ -689,6 +689,15 @@ public class ListTester {
 			//TODO: tests for scenarios ending in a 2-element list
 			// IndexedUnsortedList
 
+			//Tests by @HudsonTran1
+			printTest(scenarioName + "_testFirst", testFirst(scenario.build(), contents[0], Result.MatchingValue));
+			printTest(scenarioName + "_testLast", testLast(scenario.build(), contents[1], Result.MatchingValue));
+			printTest(scenarioName + "_testContains" + contentsString.charAt(0), testContains(scenario.build(), contents[0], Result.True));
+			printTest(scenarioName + "_testContains" + contentsString.charAt(1), testContains(scenario.build(), contents[1], Result.True));
+			printTest(scenarioName + "_testContainsX", testContains(scenario.build(), ELEMENT_X, Result.False));
+			printTest(scenarioName + "_testIsEmpty", testIsEmpty(scenario.build(), Result.False));
+			printTest(scenarioName + "_testSize", testSize(scenario.build(), 2));
+			printTest(scenarioName + "_testToString", testToString(scenario.build(), Result.ValidString));
 			// Iterator
 				// TODO: will add in Lab 10
 				// test to construct iterator
