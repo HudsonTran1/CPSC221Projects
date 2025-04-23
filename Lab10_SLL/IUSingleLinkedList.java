e38ca675c1a9ecfbeed8c1998ca38534a4ebee23
HudsonTran1 <hudsontran@my.cwi.edu>
2025-04-23 15:00:17 -0600

    Cleaned up, fixed some errors not caught by testing

--- Lab10_SLL/IUSingleLinkedList.java
+++ Lab10_SLL/IUSingleLinkedList.java
@@ -1,4 +1,3 @@
-import java.security.NoSuchAlgorithmException;
 import java.util.*;
 
 /**
@@ -25,37 +24,17 @@ public class IUSingleLinkedList<E> implements IndexedUnsortedList<E> {
 
 	@Override
 	public void addToFront(E element) {
-		LinearNode<E> tempNode = new LinearNode<E>(element); // temp node
-		tempNode.setNext(front); // set the next of the new node to point to the old front
-		if (isEmpty()) {
-			rear = tempNode; // fix rear reference if empty
-		}		
-		front = tempNode; // add new node as the front
-		tempNode = null; // null out the temp node
-		
-		count++;
-		modCount++;
-		
+		add(0, element);
 	}
 
 	@Override
 	public void addToRear(E element) {
-		add(element);
+		add(count, element);
 	}
 
 	@Override
 	public void add(E element) {
-		LinearNode<E> tempNode = new LinearNode<E>(element);
-		if (isEmpty()) {
-			front = tempNode;
-			rear = tempNode;
-		} else {
-			rear.setNext(tempNode);
-		}
-		rear = tempNode;
-		tempNode = null;
-		count++;
-		modCount++;
+		add(count, element);
 	}
 
 	@Override
@@ -67,49 +46,45 @@ public class IUSingleLinkedList<E> implements IndexedUnsortedList<E> {
 
 	@Override
 	public void add(int index, E element) {
-		if(index < 0 || index > count) { throw new IndexOutOfBoundsException(); } // guard
+		if(index < 0 || index > count) { throw new IndexOutOfBoundsException(); } // guard for indices out of bounds
 		
 		LinearNode<E> temp = new LinearNode<>(element); // new node to add
 		LinearNode<E> previous = getLinearNode(index - 1); // node preceding
 		LinearNode<E> current = getLinearNode(index); // node originally at the index
-		LinearNode<E> following = getLinearNode(index + 1);
 
 		temp.setNext(current); // point the next of the new node to the next node 
 		
-		if(previous == null) {
-			front = temp;
+		// correct front reference or set previous of new node
+		if(index == 0) {
+			front = temp; // set front to new node
 	    } else {
 			previous.setNext(temp); // point the next of the previous node to the new node
 		}
-		if(following == null) {
-			if(current == null) {
-				rear = temp;
-			} else {
-				rear = current;
-			}
+		// set rear to new node if applicable
+		if(index == count) {
+			rear = temp;
 		}
 
+		temp = null; // null out temp
 		count++;
 		modCount++;		
 	}
 
 	@Override
 	public E removeFirst() {
-		if(isEmpty()) { throw new NoSuchElementException(); }
+		if(isEmpty()) { throw new NoSuchElementException(); } // guard for empty list
 		return removeElement(null, front);
 	}
 
 	@Override
 	public E removeLast() {
-		if (isEmpty()) { throw new NoSuchElementException(); }
-		return removeElement(getLinearNode(count - 2), rear); 
+		if (isEmpty()) { throw new NoSuchElementException(); } // guard for empty list
+		return removeElement(getLinearNode(count - 2), rear); // previous here is one node before the last, which would be at [count-1]
 	}
 
 	@Override
 	public E remove(E element) {
-		if (isEmpty()) {
-			throw new NoSuchElementException();
-		}
+		if (isEmpty()) { throw new NoSuchElementException(); } // guard for empty list
 		LinearNode<E> current = front, previous = null;
 		while (current != null && !current.getElement().equals(element)) {
 			previous = current;
@@ -119,119 +94,135 @@ public class IUSingleLinkedList<E> implements IndexedUnsortedList<E> {
 		if (current == null) {
 			throw new NoSuchElementException();
 		}
-		modCount++;
+
 		return removeElement(previous, current);		
 	}
 
 	@Override
 	public E remove(int index) {
-		return remove(get(index));
+		return remove(get(index)); // remove element found with get()
 	}
 
 	@Override
 	public void set(int index, E element) {
-		if(index < 0 || index >= count) { throw new IndexOutOfBoundsException(); }
-		LinearNode<E> temp = new LinearNode<>(element);
-		LinearNode<E> previous = getLinearNode(index - 1);
-		LinearNode<E> following = getLinearNode(index + 1);
-		if(previous != null) {
-			temp.setNext(following); // point the next of the new node to the next node 
+		if(index < 0 || index >= count) { throw new IndexOutOfBoundsException(); } // guard for indices out of bounds
+
+		LinearNode<E> temp = new LinearNode<>(element); // new node to add
+		LinearNode<E> previous = getLinearNode(index - 1); // node preceding 
+		LinearNode<E> following = getLinearNode(index + 1); // node following
+
+		temp.setNext(following); // point the next of the new node to the following node 
+
+		// correct front reference or set previous of new node
+		if(index == 0) {
+			front = temp; // set front to new node
+	    } else {
 			previous.setNext(temp); // point the next of the previous node to the new node
 		}
+		// set rear to new node if applicable
+		if(index == (count - 1)) {
+			rear = temp;
+		}
 		
+		temp = null; // null out temp
 		modCount++;		
 	}
 
 	@Override
 	public E get(int index) {
-		if(index < 0 || index >= count) { throw new IndexOutOfBoundsException(); }
-		LinearNode<E> current = front;
+		if(index < 0 || index >= count) { throw new IndexOutOfBoundsException(); } // guard for indices out of bounds
+		LinearNode<E> current = front; // node to walk through the linked structure
 
+		// bump current to the next node until node at index is reached
 		for (int i = 0; i < index; i++) {
 			current = current.getNext();
 		}
 		
-		if(current == null) { throw new IndexOutOfBoundsException(); }
-		return current.getElement();
-		
+		return current.getElement(); // return the element from the appropriate node
 	}
 
 	@Override
 	public int indexOf(E element) {
-		LinearNode<E> current = this.front;
+		LinearNode<E> current = front; // node to walk through the linked structure
 		
+		// check each node for the element
 		for(int i = 0; current != null; i++) {
 			if (current.getElement().equals(element)) {
-				return i;
+				return i; // return the index of the node
 			}
-			current = current.getNext();
+			current = current.getNext(); // bump to next node
 		}
-		return NOT_FOUND;
+		return NOT_FOUND; // return constant to denote that the element was not found
 	}
 
 	// support method to find node with target element -- prevents unnecessary call into indexOf which is insignificantly more efficient but makes me happy
 	private LinearNode<E> getLinearNode(E target) {
-		LinearNode<E> current = this.front;
+		LinearNode<E> current = front; // node to walk through the linked structure
 		
+		// check each node for the element
 		while(current != null) {
 			if (current.getElement().equals(target)) {
-				return current;
+				return current; // return the node containing the element
 			}
 		}
-		return null;
+		return null; // return null if no node possesses the element
 	}
 
 	// support method to find node at index 
 	private LinearNode<E> getLinearNode(int index) {
-		if(index < 0 || index > count) { return null; }
-		LinearNode<E> current = this.front;
+		if(index < 0 || index >= count) { return null; } // guard for invalid indices
+		LinearNode<E> current = front; // node to walk through the linked structure
 		
+		// bump current to the next node until index is reached
 		for (int i = 0; i < index; i++) {
-			current = current.getNext();
+			current = current.getNext(); // bump to next node
 		}
 
-		return current;
+		return current; // return the node at the index
 	}
 
 	@Override
 	public E first() {
-		if (isEmpty()) { throw new NoSuchElementException(); } //TODO fix
+		if (isEmpty()) { throw new NoSuchElementException(); } // guard for empty list
 		return front.getElement();
 	}
 
 	@Override
 	public E last() {
-		if (isEmpty()) { throw new NoSuchElementException(); }
+		if (isEmpty()) { throw new NoSuchElementException(); } // guard for empty list
 		return rear.getElement();
 	}
 
 	@Override 
 	public boolean contains(E target) {
-		if(isEmpty()) { return false; }
-		LinearNode<E> current = this.front;
+		if(isEmpty()) { return false; } // return early if list is empty
+		LinearNode<E> current = front; // node to walk through the linked structure
+		
+		// bump current to next node until node with element is located
 		while(current != null) {
+			// check if node contains element
 			if(current.getElement().equals(target)) {
 				return true;
 			}
-			current = current.getNext();
+			current = current.getNext(); // bump to next node
 		}
 		return false;
 	}
 
 	@Override
 	public boolean isEmpty() {
-		return (count == 0);
+		return (count == 0); // check if list is empty
 	}
 
 	@Override
 	public int size() {
-		return this.count;
+		return count; // return the size
 	}
 
 	@Override
 	public String toString() {
 		String result = "[";
-		LinearNode<E> current = this.front;
+		LinearNode<E> current = front;
 		while(current != null) {
 			result += current.getElement();
 			current = current.getNext();
